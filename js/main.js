375aa0130a039f069d94e79047569b7d0dc7f7e9
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-11-05 10:01:30 +0900

    lesson10

--- js/main.js
+++ js/main.js
@@ -21,6 +21,20 @@ console.clear();
     console.log(dates);
   }
 
+  function getCalenderTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
+
+    for( let i = 1; i < 7 - lastDay; i++){
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    console.log(dates);
+  }
+
   function getCalenderBody(){
     const dates = []; //date:日付, day:曜日
     const lastDate = new Date(year, month + 1, 0).getDate();
@@ -36,5 +50,6 @@ console.clear();
   }
 
   // getCalenderBody();
-  getCalenderHead();
+  // getCalenderHead();
+  getCalenderTail();
 }
